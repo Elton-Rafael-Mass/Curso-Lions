bdb691ac0bf3a08f319304d516a3d3c6b63added
Elton Rafael <eltonespcex@gmail.com>
2026-03-25 17:15:09 -0300

    ex013 da lista de vetor

--- lista-vetor-01/ex13.js
+++ lista-vetor-01/ex13.js
@@ -1,8 +1,9 @@
 let numeros = [2, 3, 2, 5, 2, 6, 2, 2, 9, 2];
-let maiores = 0;
+let x = 2;
+let contador = 0;
 for(let i = 0; i < numeros.length; i++){
-    if(numeros[i] === 2){
-       maiores ++
+    if(numeros[i] === x){
+       contador ++
     }
 }
-console.log("apareceu", maiores, "vezes");
+console.log("apareceu", contador, "vezes");
